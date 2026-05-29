c7b72ff017fb12ab5a256ec644e2016f752e2e2b
HAFEEZAH029 <khafeezah2000@gmail.com>
2026-05-29 11:46:18 -1100

    feat: add query store and root query group

--- src/components/query-builder/QueryBuilder.tsx
+++ src/components/query-builder/QueryBuilder.tsx
@@ -1,1 +1,10 @@
+"use client";
 
+import { useQueryStore } from "@/store/query-store";
+import QueryGroup from "./QueryGroup";
+
+export default function QueryBuilder() {
+  const rootGroup = useQueryStore((state) => state.rootGroup);
+
+  return <QueryGroup group={rootGroup} isRoot />;
+}
